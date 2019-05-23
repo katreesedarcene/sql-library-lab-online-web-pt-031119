49914742dd77b264eaa8cad0c4678bf78fb4a6d2
Kat B <katreesebarnes@gmail.com>
2019-05-23 14:06:20 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,8 +26,8 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name  
   FROM authors, subgenres 
-  ON series.author_id = author_id 
-  JOIN series.subgenre_id = subgenres_id;"
+  ON series.author_id = author.id 
+  JOIN series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
