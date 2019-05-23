47271b12901e7e5292ceaea9e17ae8a4e7ca3aba
Kat B <katreesebarnes@gmail.com>
2019-05-23 12:14:56 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,4 +2,4 @@ INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Walking Dead"
 
 INSERT INTO books (id, title, year, series_id) VALUES (1, "Walk", 1999, 1), (2, "Fear", 2011, 2), (3, "Zombies", 3, 3), (4, "New World", 4, 4), (5, "Run", 5, 5), (6, "Hide", 6, 6);
 
-INSERT INTO characters (id, title, author_id, subgenre_id) VALUES (1, "Walking Dead", 1, 1), (2, "Fear of Dead", 2, 2);
+INSERT INTO characters (id, name, species_id, subgenre_id) VALUES (1, "Walking Dead", 1, 1), (2, "Fear of Dead", 2, 2);
