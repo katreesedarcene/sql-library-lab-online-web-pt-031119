5ae62d28fb16f2ea993fd844efe9e8d1ae79c3cc
Kat B <katreesebarnes@gmail.com>
2019-05-23 12:11:50 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,3 @@
 INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Walking Dead", 1, 1), (2, "Fear of Dead", 2, 2);
 
-INSERT INTO books (id, title, year, series_id) VALUES (1, "Walking Dead", 1, 1), (2, "Fear of Dead", 2, 2);
+INSERT INTO books (id, title, year, series_id) VALUES (1, "Walk", 1999, 1), (2, "Fear", 2011, 2), (3, "Zombies", 3, 3), (4, "New World", 4, 4);
